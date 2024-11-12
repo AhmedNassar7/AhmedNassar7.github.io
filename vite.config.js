b442b58b013156668efa788bbefc75fb6ce597cd
Ahmed Nassar <a.moh.nassar00@gmail.com>
2024-11-12 03:18:29 +0200

    fix: set base path for portfolio hosting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/', // This is the subdirectory where the site will be hosted
   build: {
     outDir: 'dist',
   },
